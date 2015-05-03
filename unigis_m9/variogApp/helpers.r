d25ee08c4ed05f1ded73446151aa633af9a03fd3
Gabo Gaona <gavg712@gmail.com>
2015-05-03 14:56:28 -0500

    se agregó repositorio cran por defecto

--- unigis_m9/variogApp/helpers.r
+++ unigis_m9/variogApp/helpers.r
@@ -1,4 +1,4 @@
 list.of.packages <- c("geoR")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
+if(length(new.packages)) install.packages(new.packages, repo="http://cran.rstudio.com/")
 
