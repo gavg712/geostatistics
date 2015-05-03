1739948a09ff2c9ae8d059eb220297811f68c6ac
Gabo Gaona <gavg712@gmail.com>
2015-05-03 13:33:26 -0500

    se agregó el archivo helpers

--- unigis_m9/variogApp/helpers.r
+++ unigis_m9/variogApp/helpers.r
@@ -2,6 +2,3 @@ list.of.packages <- c("geoR")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
-
-runGitHub("geostatistics", username = "gavg712", subdir = "unigis_m9/variogApp")
-
