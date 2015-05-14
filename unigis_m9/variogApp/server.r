8834223d43b84c674d4958e1b7d2950473cdf27b
Gabo Gaona <gavg712@gmail.com>
2015-05-13 22:17:27 -0500

    Agregada la opcíón para analizar los propios datos

--- unigis_m9/variogApp/server.r
+++ unigis_m9/variogApp/server.r
@@ -1,49 +1,67 @@
-source("helpers.r")
+library(shiny)
+#source("helpers.r")
 options(shiny.maxRequestSize=30*1024^2)
-shinyServer(function(input, output, session) {
-        df = read.table("data/Elevacion.txt", header=TRUE,sep=",",)
-require(geoR)
-        #Construir el dataframe para representar en el variograma
-        dt.variog <- data.frame(df[,1], df[,2], df[,3])
-        distancias <- dist(dt.variog[,1:2])
-
-# Grafico del semivariograma
-    output$plot1 <- renderPlot({
-        #Control de tipo de binneo
-        vg <- if(input$userlags == FALSE){
-                variog(coords=dt.variog[,1:2], data=dt.variog[,3], breaks="default")
-                }else{
-                    breaks =    seq(from = 0, 
-                                l = input$nlag+1, 
-                                by = max(distancias)/input$nlag)
-                    variog(coords = dt.variog[,1:2], data = dt.variog[,3], 
-                        option=c("bin"), breaks = breaks)
-                }
-        #Graficohttp://shiny.rstudio.com/reference/shiny/latest/tableOutput.html
-        plot(   vg,type = "p", pch=20,
-                main = paste("Semivariograma: Experimental + Modelo ", input$model), 
-                xlab="Distancia", ylab="Semivarianza")
-        if (input$addmodel == TRUE){
-        lines.variomodel(   cov.model=input$model, 
-                            cov.pars=c(input$sill, input$range), 
-                            nugget=input$nugget, 
-                            max.dist = 100000, 
-                            kappa=kappa,
-                            lwd = 3)
-        }
+shinyServer(function(input, output){
+  df <- reactive({
+    inFile <- input$datafile
+    if (is.null(inFile))
+      return(NULL)
+    df <- read.table(inFile$datapath, header=input$header, sep=input$sep, quote=input$quote)    
+  })
+    output$dataOut <- renderTable({
+      inFile <- input$datafile
+      if (is.null(inFile))
+        return(NULL)
+      summary(df())
     })
-    
-    output$report <- renderTable({
-        #Control de tipo de binneo
-        vg <- if(input$userlags == FALSE){
-                variog(coords=dt.variog[,1:2], data=dt.variog[,3], breaks="default")
-                }else{
-                    breaks =    seq(from = 0, 
-                                l = input$nlag+1, 
-                                by = max(distancias)/input$nlag)
-                    variog(coords = dt.variog[,1:2], data = dt.variog[,3], 
-                        option=c("bin"), breaks = breaks)
-                }
-        data.frame("Distancia.lag"=vg$u, "Semivarianza"=vg$v, "Pares"=vg$n)
-        })
+  require(geoR)
+# Grafico del semivariograma
+   output$plot1 <- renderPlot({
+     inFile <- input$datafile
+     if (is.null(inFile))
+       return(NULL)
+     #Construir el dataframe para representar en el variograma
+     db <- df()
+     dt.variog <- data.frame(db[,input$colX], db[,input$colY], db[,input$colZ])
+     distancias <- dist(dt.variog[,1:2])
+     ##Control de tipo de binneo
+     vg <- if(input$userlags == FALSE){
+       variog(coords=dt.variog[,1:2], data=dt.variog[,3], breaks="default")
+     }else{
+       breaks =    seq(from = 0, 
+                       l = input$nlag+1, 
+                       by = max(distancias)/input$nlag)
+       variog(coords = dt.variog[,1:2], data = dt.variog[,3], 
+              option=c("bin"), breaks = breaks)
+     }
+     #Grafico http://shiny.rstudio.com/reference/shiny/latest/tableOutput.html
+       plot(   vg,type = "p", pch=20,
+               main = paste("Semivariograma: Experimental + Modelo ", input$model), 
+               xlab="Distancia", ylab="Semivarianza")
+       if (input$addmodel == TRUE){
+       lines.variomodel(   cov.model=input$model, 
+                           cov.pars=c(input$sill, input$range), 
+                           nugget=input$nugget, 
+                           max.dist = 100000, 
+                           kappa=kappa,
+                           lwd = 3)
+       }
+   })
+#    
+#    output$report <- renderTable({
+#        #Control de tipo de binneo
+#        vg <- if(input$userlags == FALSE){
+#                variog(coords=dt.variog[,1:2], data=dt.variog[,3], breaks="default")
+#                }else{
+#                    breaks =    seq(from = 0, 
+#                                l = input$nlag+1, 
+#                                by = max(distancias)/input$nlag)
+#                    variog(coords = dt.variog[,1:2], data = dt.variog[,3], 
+#                        option=c("bin"), breaks = breaks)
+#                }
+#        data.frame("Distancia.lag"=vg$u, "Semivarianza"=vg$v, "Pares"=vg$n)
+#        })
+##interpolación
+
+
 })
--- unigis_m9/variogApp/ui.r
+++ unigis_m9/variogApp/ui.r
@@ -1,25 +1,42 @@
-shinyUI(fluidPage(
-    titlePanel(h1("Variografía en R",align = "center")),
+shinyUI(
+fluidPage(
+#     titlePanel(h1("Variografía en R",align = "center")),
     sidebarLayout(
-        sidebarPanel(p("Modifique los valores para ajustar el resultado"),
-            tags$hr(),
-            fluidRow(h4("Semivariograma experimental"),
-                column(4, checkboxInput('userlags', h6('Ajustar bines manualmente'), FALSE)),
-                column(8, sliderInput("nlag", label = "Número de Lags", min = 1,
-                                max = 100, value = 10))
-            ),
-            tags$hr(),
-            fluidRow(h4("Modelo teórico"),
+        sidebarPanel(width = 4,
+            fluidRow(       #InputFile desde aquí
+                fileInput(  'datafile', "Cargar datos", multiple = FALSE, 
+                        accept = c("text/csv","text/plain")),
+                fluidRow(
+                    column(8,
+                        checkboxInput('header', 'Encabezado', TRUE),
+                        column(5,   radioButtons('sep', 'Separador',
+                                c(Comma=',', Semicolon=';', Tab='\t', Space=' '),',')),
+                        column(7,   radioButtons('quote', 'Quote', c(None='',
+                                'Double Quote'='"', 'Single Quote'="'"),'"'))
+                    ),
+                    column(4, p('Núm. columna:', align = "center"),
+                        fluidRow(column(2,h6("X")),column(8,numericInput('colX', label = NULL, value = 1))),
+                        fluidRow(column(2,h6("Y")),column(8,numericInput('colY', label = NULL, value = 2))),
+                        fluidRow(column(2,h6("Z")),column(8,numericInput('colZ', label = NULL, value = 3)))
+                    )
+                )
+            ), #InputFile hasta aquí
+            fluidRow(h4("Semivariograma experimental"), #parametros smvg experimental desde
+                column(3, checkboxInput('userlags', h6('Ajustar bines manualmente'), FALSE)),
+                column(6, sliderInput("nlag", label = "Número de Lags", min = 1,
+                      max = 100, value = 10))
+            ), #parametros smvg experimental hasta
+            fluidRow(h4("Modelo teórico"), #parametros smvg teorico desde
                 column(4, 
                      checkboxInput('addmodel', h6('Agregar modelo teórico'), FALSE)
                 ),
-                column(8, 
+                column(6, 
                      selectInput("model", label = "Seleccione modelo",
-                                 choices = list("","matern", "exponential", "gaussian",
+                                 choices = list("matern", "exponential", "gaussian",
                                  "spherical", "circular", "cubic", "wave", "power",
                                  "powered.exponential", "cauchy", "gencauchy", 
                                  "gneiting", "gneiting.matern", "pure.nugget"),
-                                 selected = "")
+                                 selected = "exponential")
                 )
             ),
             fluidRow(
@@ -29,14 +46,14 @@ shinyUI(fluidPage(
             fluidRow(
                 column(6,numericInput("nugget", label = h6("Nugget"), value = 0)),
                 column(6,numericInput("kappa", label = h6("Kappa"), value = 1))
-            )
+            ) #parametros smvg teorico hasta
         ),
         mainPanel(h5(textOutput("text1"), align="center"),
             fluidRow(
-                column(7,plotOutput("plot1"),
+                column(8,plotOutput("plot1"),
                 helpText(p("Esta aplicación está orientada al aprendizaje sobre variogramas en R. Los datos utilizados fueron proporcionados por Pablo Cabrera Barona. El código y datos se encuentran disponibles en ", a("Github", href = "https://github.com/gavg712/geostatistics/tree/master/unigis_m9/variogApp")))
                 ),
-                column(4,h5("Reporte", align="center"), tableOutput("report"))
+                column(3,h5("Reporte", align="center"), tableOutput("dataOut"))
             )
         )
     )
